4fae5e0405d5d7bbba01756de0954acc111cb4a7
Sanjana Hampanal <sanjanahampanal16@gmail.com>
2026-08-13 16:56:20 +0530

    chore: prepare server for deployment

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 
